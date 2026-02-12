d62a42cad2a067fae01bc1907e1d5a8baa5e94de
Rushil Dhodra <rushild95@gmail.com>
2026-02-12 18:21:48 +0530

    Update instance creation dialog styles

--- src/components/ResourceInstance/CreateInstanceModal/CreateInstanceModal.tsx
+++ src/components/ResourceInstance/CreateInstanceModal/CreateInstanceModal.tsx
@@ -3,6 +3,7 @@ import { Box, Dialog, IconButton, Stack, styled } from "@mui/material";
 
 import Button from "src/components/Button/Button";
 import CopyToClipboardButton from "src/components/CopyClipboardButton/CopyClipboardButton";
+import TextField from "src/components/FormElementsv2/TextField/TextField";
 import InstructionsCircledIcon from "src/components/Icons/InstructionsCircled/InstructionsCircled";
 import StatusChip from "src/components/StatusChip/StatusChip";
 import { Text } from "src/components/Typography/Typography";
@@ -38,12 +39,13 @@ const Content = styled(Box)({
 });
 
 const Footer = styled(Box)({
-  marginTop: "24px",
+  marginTop: "16px",
   width: "100%",
   display: "flex",
   justifyContent: "flex-end",
   alignItems: "center",
   gap: "16px",
+  paddingTop: "24px",
 });
 
 export type CreateInstanceModalData = {
@@ -62,7 +64,7 @@ type CreateInstanceModalProps = {
 function CreateInstanceModal(props: CreateInstanceModalProps) {
   const { open, handleClose, data } = props;
 
-  const { instanceId, isCustomDNS, isFirstInstanceInRegion, lifecycleStatus = "DEPLOYING" } = data || {};
+  const { instanceId, isCustomDNS = false, isFirstInstanceInRegion = true, lifecycleStatus = "DEPLOYING" } = data || {};
 
   const statusStylesAndLabel = getResourceInstanceStatusStylesAndLabel(lifecycleStatus as string);
 
@@ -81,72 +83,60 @@ function CreateInstanceModal(props: CreateInstanceModalProps) {
           </IconButton>
         </Header>
         <Content>
-          <Text size="small" weight="semibold" color="#344054">
+          <Text size="small" weight="medium" color="#344054">
             {isFirstInstanceInRegion
               ? "Your instance has been created and is currently deploying."
               : " Your instance is being set up and will be ready shortly (usually within a few minutes)."}
           </Text>
           <Box
+            borderTop="1px solid #E9EAEB"
+            padding="16px 0px"
             display="grid"
-            gridTemplateColumns="auto auto 1fr"
-            marginTop="20px"
-            columnGap={"20px"}
-            rowGap={"10px"}
+            gridTemplateColumns="auto 1fr"
+            marginTop="16px"
+            columnGap={"64px"}
+            rowGap={"16px"}
             alignItems={"center"}
           >
-            <Text size="small" weight="semibold" color="#344054">
+            <Text size="small" weight="medium" color="#414651">
               Instance ID
             </Text>
-            <Text size="small" weight="semibold" color="#344054">
-              :
-            </Text>
+
             <Stack direction="row" alignItems="center" gap="4px">
-              <Text size="small" weight="regular" color="#535862" data-testid="instance-id">
-                {instanceId}
-              </Text>
-              {instanceId && (
-                <CopyToClipboardButton
-                  text={instanceId}
-                  iconProps={{ color: "#067647" }}
-                  tooltipText="Copy Instance ID"
-                />
-              )}
+              <TextField disabled value={instanceId} sx={{ maxHeight: "40px", marginTop: "0px", flexGrow: 1 }} />
+              {instanceId && <CopyToClipboardButton text={instanceId} tooltipText="Copy Instance ID" />}
             </Stack>
-            <Text size="small" weight="semibold" color="#344054">
+            <Text size="small" weight="medium" color="#414651">
               Lifecycle Status
             </Text>
-            <Text size="small" weight="semibold" color="#344054">
-              :
-            </Text>
+
             <Stack direction="row" alignItems="center" gap="8px">
               <StatusChip status={lifecycleStatus} {...statusStylesAndLabel} showOverflowTitle />
             </Stack>
           </Box>
 
           {(isCustomDNS || isFirstInstanceInRegion) && (
-            <Box mt="20px" borderLeft="2px solid #F79009" paddingLeft="10px" paddingTop="2px" paddingBottom="2px">
-              {isFirstInstanceInRegion && (
-                <Text size="small" weight="medium" color="#414651">
-                  The first deployment in your cloud account may take ~20 minutes. Subsequent deployments in the same
-                  region are faster.
-                </Text>
-              )}
-              {isFirstInstanceInRegion && isCustomDNS && <br />}
-              {isCustomDNS && (
-                <Text size="small" weight="medium" color="#414651">
-                  As you have provided a custom DNS, it will need to be configured with your DNS provider. The
-                  configuration details will be available after some time. Please revisit the Custom DNS tab later to
-                  access the necessary information.
-                </Text>
-              )}
+            <Box p="16px 0px" borderTop="1px solid #E9EAEB">
+              <Box borderLeft="2px solid #F79009" paddingLeft="10px" paddingTop="2px" paddingBottom="2px">
+                {isFirstInstanceInRegion && (
+                  <Text size="small" weight="medium" color="#414651">
+                    The first deployment in your cloud account may take ~20 minutes. Subsequent deployments in the same
+                    region are faster.
+                  </Text>
+                )}
+                {isFirstInstanceInRegion && isCustomDNS && <br />}
+                {isCustomDNS && (
+                  <Text size="small" weight="medium" color="#414651">
+                    As you have provided a custom DNS, it will need to be configured with your DNS provider. The
+                    configuration details will be available after some time. Please revisit the Custom DNS tab later to
+                    access the necessary information.
+                  </Text>
+                )}
+              </Box>
             </Box>
           )}
-
-          <Text size="small" weight="regular" sx={{ marginTop: "20px" }} color="#535862">
-            Track progress in Deployment Instances.
-          </Text>
         </Content>
-        <Footer sx={{ marginTop: "32px", justifyContent: "center" }}>
+        <Footer sx={{ justifyContent: "flex-end", borderTop: "1px solid #E9EAEB" }}>
           <Button
             size="large"
             variant="contained"
