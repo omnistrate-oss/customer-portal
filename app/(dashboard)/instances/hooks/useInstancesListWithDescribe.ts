19f84bce532027b85c658b2e56b3059ce77200b8
mohandholu43 <mohandholu43@gmail.com>
2026-03-14 01:39:33 +0530

    fix order

--- app/(dashboard)/instances/hooks/useInstancesListWithDescribe.ts
+++ app/(dashboard)/instances/hooks/useInstancesListWithDescribe.ts
@@ -53,11 +53,7 @@ const useInstancesListWithDescribe = (queryOptions: QueryOptions = {}) => {
   const describeQuery = useQuery({
     queryKey: ["resource-instances-describe", listQuery.dataUpdatedAt, serviceOfferings?.length],
     enabled: Boolean(
-      describeInstances &&
-        listQuery.data &&
-        !listQuery.isFetching &&
-        !listQuery.isPending &&
-        serviceOfferings?.length
+      describeInstances && listQuery.data && !listQuery.isFetching && !listQuery.isPending && serviceOfferings?.length
     ),
     queryFn: async () => {
       const res = listQuery.data || [];
