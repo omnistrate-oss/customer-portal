7d87cd9a365f57e2beef430940f039df7f8f856a
Rushil Dhodra <rushild95@gmail.com>
2026-05-18 12:23:25 +0530

    Revert master to v0.3.144

---
+++ src/components/ResourceInstance/Metrics/LoadAverageChart.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+
+import ReChartContainer from "../../ReChartContainer/ReChartContainer";
+
+function LoadAverageChart(props) {
+  const { data } = props;
+
+  return (
+    <ReChartContainer mt={2} debounce={100}>
+      <ResponsiveContainer>
+        <AreaChart
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 50,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" tickLine={false} />
+          <YAxis domain={[0, 1]} tickFormatter={(value) => `${value}`} />
+          <Tooltip
+            formatter={(value, name, props) => {
+              return `${value} | Time :${props.payload.x}`;
+            }}
+            isAnimationActive={false}
+          />
+
+          <Area
+            name="Load Average"
+            type="monotone"
+            dataKey="y"
+            stroke="#82ca9d"
+            fill="#82ca9d"
+            dot={false}
+            isAnimationActive={false}
+            label="Load Average"
+            strokeWidth={2}
+            connectNulls
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </ReChartContainer>
+  );
+}
+
+export default LoadAverageChart;
